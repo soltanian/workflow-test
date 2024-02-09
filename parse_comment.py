77babd0ab47fc991cc5cf1570aa0b7ec20187e50
soltanian <abbas.soltanian@gmail.com>
2024-02-09 10:17:07 -0500

    Update parse_comment.py

--- parse_comment.py
+++ parse_comment.py
@@ -1,9 +1,9 @@
-import requests
 import sys
 import yaml
 import re
 import os
 from subprocess import run, PIPE
+from github_utils import get_github_username_from_email
 
 def main():
     comment_body = sys.argv[1]
@@ -71,33 +71,7 @@ def main():
     # Push changes to the new branch
     run(["git", "push", "origin", branch_name], env={"GITHUB_TOKEN": pat})
 
-    ############### Find GIT USER ##############
-def get_github_username_from_email(email):
-    # GitHub API endpoint for searching users
-    search_users_url = "https://api.github.com/search/users"
-
-    # Parameters for the search query
-    params = {"q": email}
-
-    # Request to search for users with the given email
-    response = requests.get(search_users_url, params=params)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the response JSON
-        response_json = response.json()
-
-        # Check if any users were found
-        if response_json["total_count"] > 0:
-            # Get the username of the first user found
-            return response_json["items"][0]["login"]
-        else:
-            print("No GitHub user found with the provided email.")
-    else:
-        print("Failed to fetch GitHub user information.")
-
     github_username = get_github_username_from_email(Reviewer_email)
-    ############################################
 
     # Create a pull request
     pr_create_command = [
